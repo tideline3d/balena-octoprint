207b2ee2788e47b28f3f04b437606e0e5d349c14
jw59 <joshw59@gmail.com>
2021-05-17 09:46:41 -0400

    Change to publishing webcam config via command line
    Testing with manual values before switching to env vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
-  set -- octoprint --basedir /octoprint/octoprint "$@"
+  set -- octoprint --basedir /octoprint/octoprint config set --json webcam "{\"ffmpeg\":\"/usr/bin/ffmpeg\",\"snapshot\":\"http://10.1.2.101:8080/images/live.jpg\",\"stream\":\"http://10.1.2.101:8080/hls/live.stream.m3u8\"}" "$@"
 fi
 
 exec "$@"
