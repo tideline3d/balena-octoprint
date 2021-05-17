4140d13c427ca6bf5858552e6b66156ec933769f
jw59 <joshw59@gmail.com>
2021-05-17 10:59:13 -0400

    Doh, this should be over in 'run'

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-exec "octoprint config set --json webcam "{\"ffmpeg\":\"/usr/bin/ffmpeg\",\"snapshot\":\"http://10.1.2.101:8080/images/live.jpg\",\"stream\":\"http://10.1.2.101:8080/hls/live.stream.m3u8\"}""
-
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
   set -- octoprint --basedir /octoprint/octoprint "$@"
 fi
